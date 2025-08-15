1b998e4d18ee73768e8314d3a31ea24cb49d2303
dcblanton78 <dcblanton78@gmail.com>
2025-08-15 12:29:07 -1000

    Fix Vite base for GitHub Pages 5

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
+
 export default defineConfig({
   base: "/nearshore-nexus/", // <-- repo name between slashes
   plugins: [react()],
